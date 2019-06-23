bf1f8b0c1e779b66c3867413fc1654cc62305a90
yamitrvg12 <yamitrvg12@gmail.com>
2019-06-22 23:04:29 -0500

    Connect saga with the store

---
+++ src/sagas/index.js
@@ -0,0 +1,5 @@
+function* rootSaga() {
+    console.log('hello');
+};
+
+export default rootSaga;
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,22 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-underscore-dangle */
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
 import rootReducer from '../reducers';
+import rootSaga from '../sagas';
 
 const configureStore = () => {
+	const sagaMiddleware = createSagaMiddleware();
 	const store = createStore(
 		rootReducer,
-		window.__REDUX_DEVTOOLS_EXTENSION__
-            && window.__REDUX_DEVTOOLS_EXTENSION__(),
+		compose(
+			applyMiddleware(sagaMiddleware),
+			window.__REDUX_DEVTOOLS_EXTENSION__
+				&& window.__REDUX_DEVTOOLS_EXTENSION__(),
+		),
 	);
+	sagaMiddleware.run(rootSaga);
 
 	return store;
 };
